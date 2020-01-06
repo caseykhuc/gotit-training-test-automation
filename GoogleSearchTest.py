24d2f725ffdd553bd038b4032617b4d09dea80c4
Casey Khuc <khuctrang1812@gmail.com>
2020-01-06 15:44:32 +0700

    refactor: Open/Close new tab on first result

--- GoogleSearchTest.py
+++ GoogleSearchTest.py
@@ -1,21 +1,26 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import *
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 import time
 
 
-class GoogleSearchTest:
-    @staticmethod
-    def test():
+class GoogleSearchTest(unittest.TestCase):
+    def setUp(self):
         base_url = "https://google.com"
-        driver = webdriver.Chrome()
-        driver.implicitly_wait(.5)
-        # Launch
-        driver.maximize_window()
-        driver.get(base_url)
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(.5)
 
+        # Launch
+        self.driver.maximize_window()
+        self.driver.get(base_url)
+        # self.main_window = self.driver.current_window_handle
+
+    def test(self):
+        driver = self.driver
         # Search
         search_bar = driver.find_element(By.NAME, 'q')
         search_bar.send_keys("Random keyword")
@@ -24,17 +29,30 @@ class GoogleSearchTest:
         search_bar.submit()
 
         # Validate Url
-        if "q=Random+keyword" not in driver.current_url:
-            raise Exception("Invalid Url")
+        self.assertTrue("q=Random+keyword" in driver.current_url)
 
-        # Click on the first result
+        # Open the first result on new tab
         first_result = WebDriverWait(driver, 5).until(
             EC.presence_of_element_located((By.XPATH, '(//div[@class="r"])[1]/a')))
-        first_result.click()
 
-        WebDriverWait(driver, 5).until_not(
-            EC.presence_of_element_located((By.XPATH, '(//div[@class="r"])[1]/a')))
+        ActionChains(driver)\
+            .move_to_element(first_result)\
+            .key_down(Keys.COMMAND)\
+            .click(first_result)\
+            .key_up(Keys.COMMAND)\
+            .perform()
+
+        # Switch to the result - next tab on the right
+        window_handles = driver.window_handles
+        driver.switch_to.window(window_handles[len(window_handles)-1])
+        print(driver.title)
+        time.sleep(1)
+
+        # Close current tab
+        driver.close()
+
+        time.sleep(1)
 
 
-ff = GoogleSearchTest()
-ff.test()
+if __name__ == '__main__':
+    unittest.main()
